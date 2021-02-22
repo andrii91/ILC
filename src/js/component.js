e336fcf1d8c105ce8e55231d266c9c9f3da78c40
Andrii <litvin.andriy91@gmail.com>
2021-02-22 20:33:45 +0200

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -31,9 +31,7 @@ var widthLi = 170;
       })
     }*/
   });
-    if($(window).width() < 968 ){
-      $('#menu').prepend('<li><a href="/">Home</a></li>')
-    }
+
 
   // On before slide change
   $('.head-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
@@ -69,23 +67,12 @@ var widthLi = 170;
     e.preventDefault();
     $('.section_map-nav li').removeClass('active');
     $('.section_map-content .item').removeClass('active');
+    $('.section_map-point').removeClass('active');
     $(this).addClass('active');
 
     $($(this).find('a').attr('href')).addClass('active');
+    $('[data-tab="'+$(this).find('a').attr('href')+'"]').addClass('active');
   })
-
-
-
-
-  /*  $('.partners').mousemove(function (event) {
-      event = event || window.event; // кроссбраузерность
-
-      $('#brand').scrollLeft(event.offsetX)
-
-      console.log(event.offsetX);
-
-
-    })*/
 
 
 
@@ -95,14 +82,27 @@ var widthLi = 170;
 
   function initMap() {
     var element = document.getElementById('map');
-    var options = {
-      zoom: 16,
-      center: {
-        lat: 34.6981856,
-        lng: 33.0682552
-      }
+    
+    if ($(window).width() > 1023) {
+      var options = {
+        zoom: 16,
+        center: {
+          lat: 34.6981856,
+          lng: 33.0682552
+        }
 
-    };
+      };
+      
+    }else {
+      var options = {
+        zoom: 17,
+        center: {
+           lat: 34.6984635,
+          lng: 33.0607128
+        }
+
+      };
+    }
 
 
     var myMap = new google.maps.Map(element, options);
@@ -168,34 +168,6 @@ var widthLi = 170;
     initMap();
   }
 
-  $mq = $('.marquee').marquee({
-    //duration in milliseconds of the marquee
-    duration: 15000,
-    //gap in pixels between the tickers
-    gap: 50,
-    //time in milliseconds before the marquee will start animating
-    startVisible: true,
-    delayBeforeStart: 0,
-    //'left' or 'right'
-    direction: 'left',
-    //true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true
-  });
-
-
-  //stop
-  $mq.marquee('resume');
-
-  //on hover start
-  $mq.hover(function () {
-
-    $mq.marquee('pause');
-  }, function () {
-    // $mq.marquee('pause');
-    $mq.marquee('resume');
-  })
-
-
 
   $('#nav-icon').click(function () {
     $('.hamburger').toggleClass('is-active')
@@ -209,5 +181,42 @@ var widthLi = 170;
 
   });
 
+  
+  $.fn.make_carousel = function() {
+    var speed = 0;
+    var scroll = 0;
+    var container = $(this);
+    var container_w = container.width();
+    var max_scroll = container[0].scrollWidth - container.outerWidth();
+    var prev_frame = new Date().getTime();
+    container.on('mousemove', function(e) {
+        var mouse_x = e.pageX - container.offset().left;
+        var mouseperc = 100 * mouse_x / container_w;
+        speed = mouseperc - 50;
+    }).on ( 'mouseleave', function() {
+        speed = 0;
+    });
+
+    function updatescroll() {
+        var cur_frame = new Date().getTime();
+        var time_elapsed = cur_frame - prev_frame;
+        prev_frame = cur_frame;
+        if (speed !== 0) {
+            scroll += speed * time_elapsed / 50;
+            if (scroll < 0) scroll = 0;
+            if (scroll > max_scroll) scroll = max_scroll;
+            container.scrollLeft(scroll);
+        }
+        window.requestAnimationFrame(updatescroll);
+    }
+    window.requestAnimationFrame(updatescroll);
+}
+      if($(window).width() > 1200 ){
+        $("#brands").make_carousel();
+        
+      }
+      if($(window).width() < 768 ){
+      $('#menu').prepend('<li><a href="/">Home</a></li>')
+    }
 
 });
